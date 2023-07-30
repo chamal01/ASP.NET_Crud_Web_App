4bbb41d0e5200d66b2c53e006751e963340c65dd
chamal adhikaram <100332002+chamal01@users.noreply.github.com>
2023-07-30 10:33:23 +0530

    Add files via upload

---
+++ Users/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Crud.Data;
+using Crud.Model;
+
+namespace Crud.Pages.Users
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Crud.Data.CrudContext _context;
+
+        public IndexModel(Crud.Data.CrudContext context)
+        {
+            _context = context;
+        }
+
+        public IList<User> User { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.User != null)
+            {
+                User = await _context.User.ToListAsync();
+            }
+        }
+    }
+}
